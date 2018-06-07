fe7d9bdf53afa93fc778d23b784e20dab264921e
Chibwe Mweene <caleidosymmetry@gmail.com>
2018-06-07 04:33:59 +0000

    recurcive sum from-to function complete

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  console.log(fromN)
+  if(fromN === toN) {
+    return toN;
+  }
+  return toN + sum(fromN, toN - 1);
 }
 
 module.exports = sum;
